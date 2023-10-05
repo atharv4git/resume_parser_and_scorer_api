6f9055643158ba4ea94676e860ccd24ad843655b
atharv4git <atharv4study@gmail.com>
2023-10-05 18:05:08 +0530

    commit1

---
+++ extractions.py
@@ -0,0 +1,160 @@
+import re
+import pandas as pd
+
+def extract_contact_number_from_resume(text):
+    contact_number = None
+
+    # Use regex pattern to find a potential contact number
+    pattern = r"\b(?:\+?\d{1,3}[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\b"
+    match = re.search(pattern, text)
+    if match:
+        contact_number = match.group()
+
+    return contact_number
+
+def extract_email_from_resume(text):
+    email = None
+
+    # Use regex pattern to find a potential email address
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+    match = re.search(pattern, text)
+    if match:
+        email = match.group()
+
+    return email
+
+# def extract_skills_from_resume(text, skills_list):
+#     skills = []
+
+#     for skill in skills_list:
+#         pattern = r"\b{}\b".format(re.escape(skill))
+#         match = re.search(pattern, text, re.IGNORECASE)
+#         if match:
+#             skills.append(skill)
+
+#     return skills
+
+def extract_skills_from_resume(text, skills_list):
+    skills = []
+
+    # Tokenize the resume text (split it into words)
+    tokens = re.findall(r'\b\w+\b', text)
+
+    for skill in skills_list:
+        # Use a regular expression pattern with word boundaries and case-insensitive search
+        pattern = r"\b{}\b".format(re.escape(skill), re.IGNORECASE)
+        for token in tokens:
+            if re.search(pattern, token):
+                skills.append(skill)
+                break  # Stop searching for the current skill once found
+
+    return skills
+
+
+
+# def extract_name(resume_text):
+#     nlp = spacy.load('en_core_web_sm')
+#     matcher = Matcher(nlp.vocab)
+
+#     # Define name patterns
+#     patterns = [
+#         [{'POS': 'PROPN'}, {'POS': 'PROPN'}],  # First name and Last name
+#         [{'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN'}],  # First name, Middle name, and Last name
+#         [{'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN'}]  # First name, Middle name, Middle name, and Last name
+#         # Add more patterns as needed
+#     ]
+
+#     for pattern in patterns:
+#         matcher.add('NAME', patterns=[pattern])
+
+#     doc = nlp(resume_text)
+#     matches = matcher(doc)
+
+#     for match_id, start, end in matches:
+#         span = doc[start:end]
+#         return span.text
+
+#     return None
+
+
+def extract_name(resume_text):
+    # Define a regular expression pattern for names
+    name_pattern = r'([A-Z][a-z]+)(?:\s+[A-Z][a-z]+)+'
+    
+    # Search for the name pattern in the resume text
+    match = re.search(name_pattern, resume_text)
+    
+    if match:
+        return match.group(0)  # Return the matched name
+    else:
+        return None  # Return None if no name is found
+
+def extract_education_from_resume(text):
+    education = []
+
+    # List of education keywords to match against
+    education_keywords = ['Bsc', 'B. Pharmacy', 'B Pharmacy', 'Msc', 'M. Pharmacy', 'Ph.D', 'Bachelor', 'Master', 'B.Tech']
+
+    for keyword in education_keywords:
+        pattern = r"(?i)\b{}\b".format(re.escape(keyword))
+        match = re.search(pattern, text)
+        if match:
+            education.append(match.group())
+
+    return education
+
+
+def extract_internships_from_resume(text):
+    internships = []
+    roles_list = pd.read_csv("src/roles.csv")["roles"].tolist()
+    # Define keywords related to internships
+    internship_keywords = ['Internship', 'Intern', 'Trainee', 'intern', 'Junior', 'junior', 'Senior', 'senior', 'role', 'Role'] + roles_list
+
+
+    for keyword in internship_keywords:
+        pattern = r"(?i)\b{}\b".format(re.escape(keyword))
+        matches = re.finditer(pattern, text)
+        for match in matches:
+            # Extract the surrounding text as the description of the internship
+            internship_text = re.search(r'.*?(?=\n\n|\n|$)', text[match.end():], re.DOTALL)
+            if internship_text:
+                internships.append(internship_text.group().strip())
+
+    return internships
+
+
+def extract_projects_from_resume(text):
+    projects = []
+    roles_list = pd.read_csv("src/roles.csv")["roles"].tolist()
+    # Define keywords related to projects
+    project_keywords = ['Projects', 'Project', 'Work', 'Experience', 'Job', 'Role', 'role', 'MNIST', 'CNN', 'data'] + roles_list
+
+    for keyword in project_keywords:
+        pattern = r"(?i)\b{}\b".format(re.escape(keyword))
+        matches = re.finditer(pattern, text)
+        for match in matches:
+            # Extract the surrounding text as the description of the project
+            project_text = re.search(r'.*?(?=\n\n|\n|$)', text[match.end():], re.DOTALL)
+            if project_text:
+                projects.append(project_text.group().strip())
+
+    return projects
+
+
+def extract_social_profiles_from_resume(text):
+    social_profiles = {}
+
+    # Define social media platform names and their corresponding patterns
+    social_media_patterns = {
+        'LinkedIn': r'LinkedIn\s*-\s*(\S+)',
+        'Twitter': r'Twitter\s*-\s*(\S+)',
+        'GitHub': r'GitHub\s*-\s*(\S+)',
+        'Medium': r'Medium\s*-\s*(\S+)',
+    }
+
+    for platform, pattern in social_media_patterns.items():
+        match = re.search(pattern, text)
+        if match:
+            social_profiles[platform] = match.group(1)
+
+    return social_profiles
---
+++ index.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import requests
+from extractions import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from pdfminer.high_level import extract_text
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def create_resource():
+    try:
+        data = request.get_json()  # Get the JSON data from the request
+
+        # URL of the image you want to read
+        pdf_url = data['pdf_url']
+        response = requests.get(pdf_url)
+
+        def extract_text_from_pdf(pdf_url):
+            response = requests.get(pdf_url)
+            with open("resume.pdf", "wb") as pdf_file:
+                pdf_file.write(response.content)
+
+            # Extract text from the downloaded PDF
+            text = extract_text("resume.pdf")
+            return text
+        
+
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            text = extract_text_from_pdf(pdf_url)
+
+            # Store the extracted text in the dictionary
+            output_data = {}
+            output_data['text'] = text
+
+            name = extract_name(text)
+            output_data['name'] = name if name else "Name not found"
+
+            contact_number = extract_contact_number_from_resume(text)
+            output_data['contact_number'] = contact_number if contact_number else "Contact Number not found"
+
+            email = extract_email_from_resume(text)
+            output_data['email'] = email if email else "Email not found"
+
+            skills_list = ['Python', 'Data Analysis', 'Machine Learning', 'Communication', 'Marketing', 'SEO', 'Digital Marketing', 'Google Analytics', 'Facebook Ads', 'Social Media']
+            extracted_skills = extract_skills_from_resume(text, skills_list)
+            output_data['skills'] = extracted_skills if extracted_skills else "No skills found"
+
+            extracted_education = extract_education_from_resume(text)
+            output_data['education'] = extracted_education if extracted_education else "No education information found"
+
+            extracted_internships_from_resume = extract_internships_from_resume(text)
+            output_data['internships'] = extracted_internships_from_resume if extracted_internships_from_resume else "No Internships information found"
+
+            extracted_projects_from_resume = extract_projects_from_resume(text)
+            output_data['projects'] = extracted_projects_from_resume if extracted_projects_from_resume else "No Projects information found"
+
+            extracted_social_profiles_from_resume = extract_social_profiles_from_resume(text)
+            output_data['social_media'] = extracted_social_profiles_from_resume if extracted_social_profiles_from_resume else "No Social Media information found"
+
+            # Convert the dictionary to a JSON response
+            return jsonify(output_data), 201
+
+        else:
+            return jsonify({"error": f"Failed to download the image. Status code: {response.status_code}"}), 500
+
+    except Exception as e:
+        return jsonify({"error": f"Error: {str(e)}"}), 500
+
+
+# Create a TF-IDF vectorizer
+tfidf_vectorizer = TfidfVectorizer()
+
+def get_tfidf_cosine_similarity(description1, description2):
+    # Fit and transform the TF-IDF vectorizer on the descriptions
+    tfidf_matrix = tfidf_vectorizer.fit_transform([description1, description2])
+
+    # Calculate cosine similarity between the two TF-IDF vectors
+    similarity_score = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])[0][0]
+
+    return similarity_score
+
+@app.route('/score', methods=['POST'])
+def score():
+    try:
+        data = request.get_json()  # Get the JSON data from the request
+        project_description = data.get("project_description")
+        applicant_description = data.get("applicant_description")
+
+        # Calculate similarity score using TF-IDF cosine similarity
+        similarity_score = get_tfidf_cosine_similarity(project_description, applicant_description)
+
+        output_score = {"score": similarity_score}
+
+        return jsonify(output_score), 201
+
+    except Exception as e:
+        return jsonify({"error": f"Error: {str(e)}"}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
